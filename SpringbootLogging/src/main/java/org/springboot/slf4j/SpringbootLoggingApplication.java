6b00b618866aede3041e9b23dd8bce81102f2330
Subhan383 <abdulsubhan0383@gmail.com>
2020-08-09 16:57:21 +0530

    Logging Example

---
+++ SpringbootLogging/src/main/java/org/springboot/slf4j/SpringbootLoggingApplication.java
@@ -0,0 +1,30 @@
+package org.springboot.slf4j;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootLoggingApplication {
+	
+	Logger log=LoggerFactory.getLogger(SpringbootLoggingApplication.class);
+	
+	@GetMapping("/logging/{name}")
+	public String logging(@PathVariable(value="name") String name) {
+		log.debug("Request {}",name);
+		String response="Hello"+" "+name;
+		log.debug("Response {}",response);
+		return response;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLoggingApplication.class, args);
+	}
+
+}
